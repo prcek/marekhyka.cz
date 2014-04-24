4f984d82efd0ec386f039c745f22401819f996b6
Tomas Hluchan <tomas.hluchan@gmail.com>
2014-04-24 09:19:54 +0200

    update hook

--- git_hook.php
+++ git_hook.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 try
 {
   $payload = json_decode($_REQUEST['payload']);
@@ -8,6 +9,12 @@ catch(Exception $e)
   exit(0);
 }
 
-//log the request
 file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
+
+
+if ($payload->ref === 'refs/heads/master')
+{
+  exec('./update.sh');
+}
+
 ?>
