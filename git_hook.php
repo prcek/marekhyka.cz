95cc0bd1321f227683d1f1c261599063467f7f0a
Prcek <tomas.hluchan@gmail.com>
2014-04-24 07:42:41 +0000

    uprava hooku

--- git_hook.php
+++ git_hook.php
@@ -6,15 +6,17 @@ try
 }
 catch(Exception $e)
 {
+  print $e;
   exit(0);
 }
 
 file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
 
-
-if ($payload->ref === 'refs/heads/master')
+if ($payload->ref == 'refs/heads/master')
 {
+  print "update triggered";
   exec('./update.sh');
+} else {
 }
 
 ?>
