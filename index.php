17b73f00cd32ed97e4e0938530b487757e771f4e
Tomas Hluchan <tomas.hluchan@gmail.com>
2015-05-24 14:59:27 +0200

    test

--- index.php
+++ index.php
@@ -136,7 +136,8 @@ end of MWS include -->
 <div class="container visible-lg visible-md" id="bgtest">
 	<div class="row">
             <div class="col-md-offset-6 col-md-5">
-               <!-- <img src="imgs/extra_2_sm_tr.png" class="img-responsive" alt="extra"> -->
+                <img src="imgs/extra_2_sm_tr.png" class="img-responsive" alt="extra"> 
+               <!-- <img src="imgs/sugo_marek.png" class="img-responsive" alt="sugo"> -->
             </div>
 	</div>
 </div>
